891ce4f17967348ab5f24245ab332652a2a1b39b
hauvan1416 <hauvan1416gmail.com>
2020-09-17 11:50:15 +0700

    carausel update

--- app.js
+++ app.js
@@ -13,31 +13,29 @@ carauselSlide.style.transform = 'translateX(' + (-size * counter) +'px)';
 
 //buttonListeners
 nextBtn.addEventListener('click',() => {
-   /*  if(counter >= carauselImages.length - 1) return;  */
+      if(counter >= carauselImages.length - 1) return;   
     carauselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
     carauselSlide.style.transform = 'translateX(' + (-size * counter) +'px)';
     
 });
  prevBtn.addEventListener('click',() => {
-     /* if(counter <=0) return;  */
+      if (counter <= 0) return;   
     carauselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
     carauselSlide.style.transform = 'translateX(' + (-size * counter) +'px)';
 
 }); 
   carauselSlide.addEventListener('transitionend', () => {
-      console.log('Fired');
+      
     if (carauselImages[counter].id === 'lastClone'){
         carauselSlide.style.transition = "none";
         counter = carauselImages.length-2;
-        
         carauselSslide.style.transform = 'translateX(' + (-size * counter) +'px)';
     }
-    
-      if(carauselImages[counter].id === 'firstClone'){
+     if (carauselImages[counter].id === 'firstClone'){
         carauselSlide.style.transition = "none";
         counter = carauselImages.length - counter;
         carauselSslide.style.transform = 'translateX(' + (-size * counter) +'px)';
-    }  
+    }   
 });  
